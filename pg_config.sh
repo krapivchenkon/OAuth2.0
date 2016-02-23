a9117e269b0e57b40e42f4614cd95bda96a7049d
krapivchenkon <krapivchenkon@gmail.com>
2016-02-23 12:57:24 +0200

    OAuth2 Udacity Course coursework

--- pg_config.sh
+++ pg_config.sh
@@ -2,6 +2,8 @@ apt-get -qqy update
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-sqlalchemy
 apt-get -qqy install python-pip
+apt-get -qqy install sqlite3 libsqlite3-dev
+
 pip install werkzeug==0.8.3
 pip install flask==0.9
 pip install Flask-Login==0.1.3
